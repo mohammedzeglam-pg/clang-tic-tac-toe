6f9b68b15a4f877a472291d742150a9bdbff964f
Mohammed Zeglam <mohammedzeglam@gmail.com>
2026-06-23 23:45:20 +0200

    format and remove macros.

--- main.c
+++ main.c
@@ -13,34 +13,19 @@
 #define O_BF "\x1b[34m"
 #define RESET "\x1b[0m"
 
-#define EVEN_OR_ODD(x, do_even, do_odd)                                        \
-  if (x % 2 == 0) {                                                            \
-    do_even;                                                                   \
-  } else {                                                                     \
-    do_odd;                                                                    \
-  }
-
-#define RETURN_IF_ALL_EQUAL(x, y, z)                                           \
-  if ((x == y) && (x == z)) {                                                  \
-    return x;                                                                  \
-  }
-
 void draw_grid(int g[]) {
   for (int x = 0; x < 9; x++) {
+    bool even = x % 2 == 0;
     switch (g[x]) {
     case O_SQUARE:
-      EVEN_OR_ODD(x, printf("%s O %s", O_BF_WB, RESET),
-                  printf("%s O %s", O_BF, RESET));
-
+      printf("%s O %s", even ? O_BF_WB : O_BF, RESET);
       break;
     case X_SQUARE:
-      EVEN_OR_ODD(x, printf("%s X %s", X_RF_WB, RESET),
-                  printf("%s X %s", X_RF, RESET));
+      printf("%s X %s", even ? X_RF_WB : X_RF, RESET);
       break;
     case BLANK_SQUARE:
     default:
-      EVEN_OR_ODD(x, printf("%s %d %s", BLANK_GF_WB, x + 1, RESET),
-                  printf("%s %d %s", BLANK_GF, x + 1, RESET));
+      printf("%s %d %s", even ? BLANK_GF_WB : BLANK_GF, x + 1, RESET);
     }
     if ((x + 1) % 3 == 0) {
       putchar('\n');
@@ -56,14 +41,14 @@ int prompt(int p, int grid[]) {
   // FIXME: proper handling user input `fgets/3`.
   scanf("%d", &square);
   if (square < 0 || square > 9) {
-    puts("Value out range");
+    puts("Value out range.");
     return -1;
   }
   if (square == 0) {
     return square;
   }
   if (grid[square - 1] != BLANK_SQUARE) {
-    printf("Square %d is occupied, try again\n", square);
+    printf("Square %d is occupied, try again.\n", square);
     return -1;
   }
   return square;
@@ -71,22 +56,35 @@ int prompt(int p, int grid[]) {
 
 int calculate_winner(int grid[]) {
   // ROWS
-  RETURN_IF_ALL_EQUAL(grid[0], grid[1], grid[2]);
-  RETURN_IF_ALL_EQUAL(grid[3], grid[4], grid[5]);
-  RETURN_IF_ALL_EQUAL(grid[6], grid[7], grid[8]);
+  if (grid[0] == grid[1] && grid[1] == grid[2]) /* First Row */
+    return grid[0];
+  if (grid[3] == grid[4] && grid[4] == grid[5]) /* Second Row */
+    return grid[3];
+  if (grid[6] == grid[7] && grid[7] == grid[8]) /* Third Row */
+    return grid[6];
+
   // COLUMNS
-  RETURN_IF_ALL_EQUAL(grid[0], grid[3], grid[6]);
-  RETURN_IF_ALL_EQUAL(grid[1], grid[4], grid[7]);
-  RETURN_IF_ALL_EQUAL(grid[2], grid[5], grid[8]);
+  if (grid[0] == grid[3] && grid[3] == grid[6]) /* First Column */
+    return grid[0];
+  if (grid[1] == grid[4] && grid[4] == grid[7]) /* Second Column */
+    return grid[1];
+  if (grid[2] == grid[5] && grid[5] == grid[8]) /* Third Column */
+    return grid[2];
+
   // DIAGONAL
-  RETURN_IF_ALL_EQUAL(grid[0], grid[4], grid[8]);
-  RETURN_IF_ALL_EQUAL(grid[2], grid[4], grid[6]);
+  if (grid[0] == grid[4] && grid[4] == grid[8]) /* Main Diagonal */
+    return grid[0];
+  if (grid[2] == grid[4] && grid[4] == grid[6]) /* Anti Diagonal */
+    return grid[1];
 
   return 0;
 }
 
 int main(void) {
-  int grid[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+  int grid[] = {BLANK_SQUARE, BLANK_SQUARE, BLANK_SQUARE,
+                BLANK_SQUARE, BLANK_SQUARE, BLANK_SQUARE,
+                BLANK_SQUARE, BLANK_SQUARE, BLANK_SQUARE};
+
   puts("Tic Tac Toe");
 
   int ply = 0, p;
